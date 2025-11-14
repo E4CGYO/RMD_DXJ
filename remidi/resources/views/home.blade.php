5fea61708b3aa6f6d239f5e2e37248f777f6175e
E4CGYO <didanpradifta@gmail.com>
2025-11-14 20:50:21 +0800

    risolved by didan

--- remidi/resources/views/home.blade.php
+++ remidi/resources/views/home.blade.php
@@ -5,5 +5,23 @@
 @section('content')
 {{-- content  --}}
 <h1>Welcome to the Home Page</h1>
+{{-- Universitas bumigora --}}
+<img src="img/ubgg.jpg" class="img-fluid" alt="fotonya kosong">
+<br>
+{{-- Universitas Mataram --}}
+<img src="img/unram.jpeg" class="img-fluid" alt="fotonya kosong">
+<br>
+{{-- UIN (State Islamic University of Mataram) --}}
+<img src="img/UIN.jpg" class="img-fluid" alt="fotonya kosong">
+<br>
+{{-- Undikma (Universitas Pendidikan Mandalika) --}}
+<img src="img/undikma.jpg" class="img-fluid" alt="fotonya kosong">
+<br>
+{{-- Universitas Nahdlatul Wathan Mataram --}}
+<img src="img/unw.jpg" class="img-fluid" alt="fotonya kosong">
+<br>
+{{-- Unitar (Universitas Islam Al-Azhar) --}}
+<img src="img/UNITAR.jpg" class="img-fluid" alt="fotonya kosong">
+<br>
 {{-- content end --}}
 @endsection
